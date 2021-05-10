f01d8e436f3a5c5fb1a8bc4e34303ad67d7d6555
Matt Solomon <matt@mattsolomon.dev>
2021-05-10 15:56:09 -0700

    Add CompoundInvariant trigger contract

--- test/CompoundInvariant.test.ts
+++ test/CompoundInvariant.test.ts
@@ -1,27 +1,38 @@
 import hre, { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { Artifact } from 'hardhat/types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
-import { CompoundInvariant } from '../typechain/CompoundInvariant';
+import { MockCToken, CompoundInvariant } from '../typechain';
 
 const { deployContract } = hre.waffle;
 
-describe('Unit tests', function () {
-  let signers: SignerWithAddress[];
+describe('CompoundInvariant', function () {
+  let deployer: SignerWithAddress, recipient: SignerWithAddress;
+  let mockCUsdc: MockCToken;
   let compoundInvariant: CompoundInvariant;
 
-  before(async function () {
-    signers = await hre.ethers.getSigners();
+  before(async () => {
+    [deployer, recipient] = await hre.ethers.getSigners();
   });
 
-  describe('CompoundInvariant', function () {
-    beforeEach(async () => {
-      const greeterArtifact: Artifact = await hre.artifacts.readArtifact('CompoundInvariant');
-      compoundInvariant = <CompoundInvariant>await deployContract(signers[0], greeterArtifact);
-    });
+  beforeEach(async () => {
+    // Deploy Mock CToken
+    const mockCTokenArtifact = await hre.artifacts.readArtifact('MockCToken');
+    mockCUsdc = <MockCToken>await deployContract(deployer, mockCTokenArtifact);
 
-    it('should deploy', async () => {
-      expect(compoundInvariant.address).to.not.equal(ethers.constants.AddressZero);
-    });
+    // Deploy  CompoundInvariant trigger
+    const triggerParams = [
+      'Compound Invariant Trigger', // name
+      'COMP-INV-TRIG', // symbol
+      'Triggers when the Compound invariant that reserves + supply = cash + borrows is violated', // description
+      [4], // platform ID for Compound
+      recipient.address, // subsidy recipient
+      mockCUsdc.address, // address of the Compound CToken market this trigger checks
+    ];
+    const compoundInvariantArtifact = await hre.artifacts.readArtifact('CompoundInvariant');
+    compoundInvariant = <CompoundInvariant>await deployContract(deployer, compoundInvariantArtifact, triggerParams);
+  });
+
+  it('should deploy', async () => {
+    expect(compoundInvariant.address).to.not.equal(ethers.constants.AddressZero);
   });
 });
