f39d5a2961bc83ead3a9d4050e3bc1ec84370d71
Matt Solomon <matt@mattsolomon.dev>
2021-06-02 15:47:52 -0700

    Add compound exchange rate trigger condition and checks

--- test/CompoundExchangeRate.test.ts
+++ test/CompoundExchangeRate.test.ts
@@ -24,7 +24,7 @@ describe('CompoundExchangeRate', function () {
     // Deploy  CompoundExchangeRate trigger
     triggerParams = [
       'Compound Exchange Rate Trigger', // name
-      'COMP-INV-TRIG', // symbol
+      'COMP-ER-TRIG', // symbol
       'Triggers when the Compound exchange rate decreases', // description
       [4], // platform ID for Compound
       recipient.address, // subsidy recipient
@@ -34,99 +34,83 @@ describe('CompoundExchangeRate', function () {
     trigger = <CompoundExchangeRate>await deployContract(deployer, compoundExchangeRateArtifact, triggerParams);
   });
 
-  it('deployment: should not deploy if market is already triggered', async () => {
-    // Break invariant of the CToken
-    await mockCUsdc.set(formatBytes32String('cash'), 0);
-    expect(await mockCUsdc.getCash()).to.equal(0);
-
-    // Try deploying the trigger
-    const compoundExchangeRateArtifact = await artifacts.readArtifact('CompoundExchangeRate');
-    await expect(deployContract(deployer, compoundExchangeRateArtifact, triggerParams)).to.be.revertedWith(
-      'Market already triggered'
-    );
+  describe('Deployment', () => {
+    it('initializes properly', async () => {
+      expect(await trigger.name()).to.equal(triggerParams[0]);
+      expect(await trigger.symbol()).to.equal(triggerParams[1]);
+      expect(await trigger.description()).to.equal(triggerParams[2]);
+      const platformIds = (await trigger.getPlatformIds()).map((id) => id.toNumber());
+      expect(platformIds).to.deep.equal(triggerParams[3]); // use `.deep.equal` to compare array equality
+      expect(await trigger.recipient()).to.equal(triggerParams[4]);
+      expect(await trigger.market()).to.equal(triggerParams[5]);
+      expect(await trigger.tolerance()).to.equal('100');
+    });
   });
 
-  it('checkAndToggleTrigger: does nothing when called on a valid market', async () => {
-    expect(await trigger.isTriggered()).to.be.false;
-    await trigger.checkAndToggleTrigger();
-    expect(await trigger.isTriggered()).to.be.false;
-  });
+  describe('checkAndToggleTrigger', () => {
+    it('does nothing when called on a valid market', async () => {
+      expect(await trigger.isTriggered()).to.be.false;
+      await trigger.checkAndToggleTrigger();
+      expect(await trigger.isTriggered()).to.be.false;
+    });
 
-  it('checkAndToggleTrigger: toggles trigger when called on a broken market', async () => {
-    expect(await trigger.isTriggered()).to.be.false;
-    await mockCUsdc.set(formatBytes32String('totalSupply'), 0);
-    expect(await trigger.isTriggered()).to.be.false; // trigger not updated yet, so still expect false
-    const tx = await trigger.checkAndToggleTrigger();
-    await expect(tx).to.emit(trigger, 'TriggerActivated');
-    expect(await trigger.isTriggered()).to.be.true;
-  });
+    it('toggles trigger when called on a broken market', async () => {
+      expect(await trigger.isTriggered()).to.be.false;
 
-  it('checkAndToggleTrigger: returns a boolean with the value of isTriggered', async () => {
-    // Deploy our helper contract for testing, which has a state variable called isTriggered that stores the last
-    // value returned from trigger.checkAndToggleTrigger()
-    const mockCozyTokenArtifact = await artifacts.readArtifact('MockCozyToken');
-    const mockCozyToken = <MockCozyToken>await deployContract(deployer, mockCozyTokenArtifact, [trigger.address]);
-    expect(await mockCozyToken.isTriggered()).to.be.false;
+      await mockCUsdc.set(formatBytes32String('exchangeRateStored'), 1);
+      expect(await trigger.isTriggered()).to.be.false; // trigger not updated yet, so still expect false
 
-    // Break the CToken
-    await mockCUsdc.set(formatBytes32String('totalReserves'), 0);
-    await mockCozyToken.checkAndToggleTrigger();
-    expect(await mockCozyToken.isTriggered()).to.be.true;
-  });
+      const tx = await trigger.checkAndToggleTrigger();
+      await expect(tx).to.emit(trigger, 'TriggerActivated');
+      expect(await trigger.isTriggered()).to.be.true;
+    });
 
-  it('tolerance: only considers invariant as violated when > 0.000001% error', async () => {
-    // Duplicate the logic from the isMarketTriggered method
-    const getPercent = (reserves: bigint, borrows: bigint, supply: bigint, exchangeRate: bigint, cash: bigint) => {
-      const lhs = (supply * exchangeRate) / WAD + reserves;
-      const rhs = cash + borrows;
-      const diff = lhs > rhs ? lhs - rhs : rhs - lhs;
-      const denominator = lhs < rhs ? lhs : rhs;
-      return (diff * WAD) / denominator;
-    };
+    it('returns a boolean with the value of isTriggered', async () => {
+      // Deploy our helper contract for testing, which has a state variable called isTriggered that stores the last
+      // value returned from trigger.checkAndToggleTrigger()
+      const mockCozyTokenArtifact = await artifacts.readArtifact('MockCozyToken');
+      const mockCozyToken = <MockCozyToken>await deployContract(deployer, mockCozyTokenArtifact, [trigger.address]);
+      expect(await mockCozyToken.isTriggered()).to.be.false;
 
-    // Read the tolerance from the contract and verify the value
-    const tolerance = (await trigger.tolerance()).toBigInt();
-    expect(tolerance).to.equal(10n ** 12n); // 10^12 = 10^-6 * 10^18 = 0.000001%
+      // Break the CToken
+      await mockCUsdc.set(formatBytes32String('exchangeRateStored'), 1);
+      await mockCozyToken.checkAndToggleTrigger();
+      expect(await mockCozyToken.isTriggered()).to.be.true;
+    });
 
-    // Use the same values we initialize the MockCToken with
-    const WAD = 10n ** 18n;
-    let totalReserves = 5359893964073n; // units of USDC
-    let totalBorrows = 3681673803163527n; // units of USDC
-    let totalSupply = 20287132947568793418n; // units of cUSDC
-    let exchangeRateStored = 219815665774648n; // units of 10^(18 + underlyingDecimals - 8)
-    let cash = 783115726329188n; // units of USDC
+    it('properly accounts for tolerance', async () => {
+      // Modify the currently stored exchange rate by a set tolerance
+      async function modifyLastExchangeRate(amount: bigint) {
+        const lastExchangeRate = (await trigger.lastExchangeRate()).toBigInt();
+        const newExchangeRate = lastExchangeRate + amount;
+        await mockCUsdc.set(formatBytes32String('exchangeRateStored'), newExchangeRate);
+        expect(await mockCUsdc.exchangeRateStored()).to.equal(newExchangeRate);
+      }
 
-    // Calculate the initial percent error using the same method as the contract
-    const percent1 = getPercent(totalReserves, totalBorrows, totalSupply, exchangeRateStored, cash);
+      // Executes checkAndToggleTrigger and verifies the expected state
+      async function assertTriggerStatus(status: boolean) {
+        await trigger.checkAndToggleTrigger();
+        expect(await trigger.isTriggered()).to.equal(status);
+      }
 
-    // This percent should be smaller than our tolerance
-    expect(percent1 < tolerance).to.be.true;
+      // Read the trigger's tolerance
+      const tolerance = (await trigger.tolerance()).toBigInt();
 
-    // Because it's within tolerance, trigger should not toggle
-    await trigger.checkAndToggleTrigger();
-    expect(await trigger.isTriggered()).to.be.false;
+      // Increase exchange rate to a larger value, should NOT be triggered (sanity check)
+      await modifyLastExchangeRate(100n);
+      await assertTriggerStatus(false);
 
-    // Define new values that put the error slightly above our tolerance
-    totalReserves = 10000000000n;
-    totalBorrows = 10000n;
-    totalSupply = 10000000000n;
-    exchangeRateStored = 100000n;
-    cash = 10000000001n;
-    await mockCUsdc.set(formatBytes32String('totalReserves'), totalReserves.toString());
-    await mockCUsdc.set(formatBytes32String('totalBorrows'), totalBorrows.toString());
-    await mockCUsdc.set(formatBytes32String('totalSupply'), totalSupply.toString());
-    await mockCUsdc.set(formatBytes32String('exchangeRateStored'), exchangeRateStored.toString());
-    await mockCUsdc.set(formatBytes32String('cash'), cash.toString());
+      // Decrease exchange rate by an amount less than tolerance, should NOT be triggered
+      await modifyLastExchangeRate(tolerance - 1n);
+      await assertTriggerStatus(false);
 
-    const percent2 = getPercent(totalReserves, totalBorrows, totalSupply, exchangeRateStored, cash);
-    expect(percent2 > tolerance).to.be.true;
-    // Our tolerance of 0.000001% = 1000000000000 as a wad
-    // New percent error =          1000100000000 as a wad
-    // Therefore difference =           100000000 as a wad
-    expect(percent2 - tolerance).to.equal(100000000n);
+      // Decrease exchange rate by an amount exactly equal to tolerance, should NOT be triggered
+      await modifyLastExchangeRate(-tolerance);
+      await assertTriggerStatus(false);
 
-    // Because it's outside tolerance, trigger should toggle
-    await trigger.checkAndToggleTrigger();
-    expect(await trigger.isTriggered()).to.be.true;
+      // Decrease exchange rate by an amount more than tolerance, should be triggered
+      await modifyLastExchangeRate(-tolerance - 1n);
+      await assertTriggerStatus(true);
+    });
   });
 });
