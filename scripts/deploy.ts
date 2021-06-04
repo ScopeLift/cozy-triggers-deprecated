00304cdd89091255e858dd6e2c5d3dd3e922c9f8
Matt Solomon <matt@mattsolomon.dev>
2021-06-04 13:25:05 -0700

    Setup deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,92 @@
 import { Contract, ContractFactory } from 'ethers';
-// We require the Hardhat Runtime Environment explicitly here. This is optional but useful for running the script in
-// a standalone fashion through `node <script>`. When running the script with `hardhat run <script>` you'll find the
-// Hardhat Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
+import axios from 'axios';
+import chalk from 'chalk';
+import readline from 'readline';
 
+const { BigNumber } = ethers;
+const { formatUnits } = ethers.utils;
+
+// Helper method to fetch JSON
+const fetch = (url: string) => axios.get(url).then((res) => res);
+
+// Get latest mainnet gas price
+async function getGasPrice() {
+  try {
+    const response = await fetch('https://www.gasnow.org/api/v3/gas/price');
+    const gasPrice = BigNumber.from(response.data.data.rapid).mul('110').div('100'); // bump returned gas price by 10%
+    console.log(chalk.dim(`  Using gasPrice of ${formatUnits(gasPrice, 'gwei')} gwei`));
+    return gasPrice;
+  } catch (e) {
+    // Gas price to fallback to if API call in getGasPrice() is not available
+    const fallbackGasPrice = BigNumber.from('50000000000'); // 50 gwei
+    console.log(`Could not fetch gas price. Using fallback gas price ${formatUnits(fallbackGasPrice, 'gwei')} gwei`);
+    return fallbackGasPrice;
+  }
+}
+
+// Helper method for waiting on user input. Source: https://stackoverflow.com/a/50890409
+const waitForInput = (query: string) => {
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+  return new Promise((resolve) =>
+    rl.question(query, (ans) => {
+      rl.close();
+      resolve(ans);
+    })
+  );
+};
+
+// Logging helper methods
+const logError = (msg: string) => console.log(`  ${chalk.red('\u2717')} Error: ${msg}. See above for details`); // \u2717 = x symbol
+const logHeader = (header: string) => console.log(chalk.magenta.bold(`\n${header}`));
+const logDeploy = (contractName: string, address: string) =>
+  console.log(`  ${chalk.green('\u2713')} Deployed: ${contractName} to ${address}`); // \u2713 = check symbol
+
+// Main deployment
 async function main(): Promise<void> {
-  // Hardhat always runs the compile task when running scripts through it. If this runs in a standalone fashion you
-  // may want to call compile manually to make sure everything is compiled
-  // await run("compile");
+  // SETUP
+  let deployerPrivateKey: string;
+  if (!process.env.DEPLOYER_PRIVATE_KEY) throw new Error('Please set your DEPLOYER_PRIVATE_KEY in a .env file');
+  else deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
+  const deployerWallet = new ethers.Wallet(deployerPrivateKey);
 
+  // VERIFICATION
+  // Verify sure the user is ok with the provided inputs
+  console.log(chalk.bold.yellow('\nPLEASE VERIFY THE BELOW PARAMETERS\n'));
+  console.log(`  Deployer address:    ${deployerWallet.address}`);
+  console.log(`  Deploying to:        ${network.name}\n`);
+  const response = await waitForInput('Do you want to continue with deployment? y/N\n');
+  if (response !== 'y') {
+    logError('User chose to cancel deployment. Exiting script');
+    return;
+  }
+  console.log(`  ${chalk.green('\u2713')} Continuing with deployment...\n`);
+
+  // DEFINE PARAMETERS
   // Define constructor parameters for CompoundExchangeRate trigger
-  const triggerParams = [
-    'Compound Exchange Rate Trigger', // name
-    'COMP-ER-TRIG', // symbol
-    'Triggers when the Compound exchange rate decreases', // description
+  const compoundTriggerParams = [
+    'Compound USDC Exchange Rate Trigger', // name
+    'cUSDC-ER-TRIG', // symbol
+    'Triggers when the Compound USDC token exchange rate decreases', // description
     [4], // platform ID for Compound
-    '0x0000000000000000000000000000000000000000', // TODO set subsidy recipient
-    '0x0000000000000000000000000000000000000000', // TODO set address of the Compound CToken market this trigger checks
+    deployerWallet.address, // subsidy recipient is deployer
+    '0x39AA39c021dfbaE8faC545936693aC917d5E7563', // This is the cUSDC address
   ];
 
-  // We get the contract to deploy
-  // NOTE: This will fail until a real CToken address is set because of the exchangeRateStored() call in the constructor
-  const CompoundExchangeRate: ContractFactory = await ethers.getContractFactory('CompoundExchangeRate');
-  const compoundExchangeRate: Contract = await CompoundExchangeRate.deploy(...triggerParams);
-  await compoundExchangeRate.deployed();
+  // EXECUTE DEPLOYMENTS
+  logHeader('Deployments');
 
-  console.log('CompoundExchangeRate deployed to: ', compoundExchangeRate.address);
+  // Deploy the contract
+  // NOTE: This will fail unless CToken address parameter is real on mainnet, because of the exchangeRateStored() call
+  //in the constructor
+  const CompoundExchangeRate: ContractFactory = await ethers.getContractFactory('CompoundExchangeRate');
+  const compoundExchangeRate: Contract = await CompoundExchangeRate.deploy(...compoundTriggerParams);
+  await compoundExchangeRate.deployed();
+  logDeploy('CompoundExchangeRate USDC Trigger', compoundExchangeRate.address);
+
+  // DONE
+  console.log(chalk.green.bold('\n🎉 🥳 🎉 Deployment complete! 🎉 🥳 🎉'));
+  console.log('Be sure to save off the above trigger address somewhere!\n');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
